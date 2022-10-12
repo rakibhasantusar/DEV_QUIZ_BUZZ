7008c89d980693249cbe8de9472ce493c05d4571
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-10-12 12:42:12 +0600

    statistics rechart and quiz few correction done

--- src/Component/Statistics.jsx
+++ src/Component/Statistics.jsx
@@ -8,10 +8,10 @@ const Statistics = () => {
 	console.log(data);
 
 	return (
-		<section className='flex items-center h-screen p-16 bg-gray-100 text-gray-900 mx-auto sm:py-12 lg:py-24 lg:flex-row'>
-			<div className='container w-full h-full flex flex-col items-center justify-center px-5 mx-auto my-8'>
+		<section className='flex items-center h-screen p-14 bg-gray-100 text-gray-900 mx-auto sm:py-12 lg:py-24 lg:flex-row'>
+			<div className='container w-[80%] h-[80%] flex flex-col items-center justify-center px-5 mx-auto my-8'>
 				<div className='m-9'>
-					<h1 className='text-4xl'>Chart for total quiz</h1>
+					<h1 className='text-4xl font-bold bg-slate-800 text-white p-3 rounded'>Chart for total quiz</h1>
 				</div>
 				<ResponsiveContainer>
 					<LineChart
@@ -21,8 +21,9 @@ const Statistics = () => {
 						<YAxis />
 						<Legend />
 						<Tooltip />
-						<Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 8 }} />
-						<Line type="monotone" dataKey="id" stroke="#82ca9d" />
+						<CartesianGrid strokeDasharray="5 5" />
+						<Line type="monotone" dataKey="total" stroke="#8884d8" activeDot={{ r: 10 }} strokeWidth={2} />
+						{/* <Line type="monotone" dataKey="id" stroke="#82ca9d" /> */}
 					</LineChart>
 				</ResponsiveContainer>
 			</div>
